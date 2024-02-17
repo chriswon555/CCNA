862e6c5ce29aec8762574ca086aaca4aa557a319
Chris <chriswon555@gmail.com>
2024-02-17 16:54:10 -0700

    Moved auth to separate file and refactored

--- cisco_auth.py
+++ cisco_auth.py
@@ -1,22 +1,9 @@
 import requests
 from requests.auth import HTTPBasicAuth
 from icecream import ic
+from auth import get_auth_token
 
 requests.packages.urllib3.disable_warnings()
-
-DNA_CENTER = {
-    "host": "sandboxdnac.cisco.com",
-    "port" : "443",
-    "username": "devnetuser",
-    "password": "Cisco123!"
-}
-
-def get_auth_token():
-    endpoint = '/dna/system/api/v1/auth/token'
-    url = 'https://' + DNA_CENTER['host'] + endpoint
-    resp = requests.post(url, auth=HTTPBasicAuth(DNA_CENTER['username'], DNA_CENTER['password']), verify=False)
-    token = resp.json()['Token']
-    return token
 
 def get_device_list():
     """
--- get_device_id.py
+++ get_device_id.py
@@ -1,19 +1,8 @@
 import requests
 from requests.auth import HTTPBasicAuth
+from auth import get_auth_token
 
-DNA_CENTER = {
-    "host": "sandboxdnac.cisco.com",
-    "port" : "443",
-    "username": "devnetuser",
-    "password": "Cisco123!"
-}
-
-def get_auth_token():
-    endpoint = '/dna/system/api/v1/auth/token'
-    url = 'https://' + DNA_CENTER['host'] + endpoint
-    resp = requests.post(url, auth=HTTPBasicAuth(DNA_CENTER['username'], DNA_CENTER['password']), verify=False)
-    token = resp.json()['Token']
-    return token
+requests.packages.urllib3.disable_warnings()
 
 def get_device_list():
     """
